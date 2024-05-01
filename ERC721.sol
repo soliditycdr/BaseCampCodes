c098a612d75845d01b119e5e17c98e6a3e0ecb07
Devos <59892387+soliditycdr@users.noreply.github.com>
2024-05-01 17:35:44 +0600

    Update ERC721.sol

--- ERC721.sol
+++ ERC721.sol
@@ -6,128 +6,128 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 // Interface for interacting with a submission contract
 interface ISubmission {
-    // Struct representing a haiku
-    struct Haiku {
-        address author; // Address of the haiku author
-        string line1; // First line of the haiku
-        string line2; // Second line of the haiku
-        string line3; // Third line of the haiku
+    // Struct representing a Devo
+    struct Devo {
+        address author; // Address of the Devo author
+        string line1; // First line of the Devo
+        string line2; // Second line of the Devo
+        string line3; // Third line of the Devo
     }
 
-    // Function to mint a new haiku
-    function mintHaiku(
+    // Function to mint a new Devo
+    function mintDevo(
         string memory _line1,
         string memory _line2,
         string memory _line3
     ) external;
 
-    // Function to get the total number of haikus
+    // Function to get the total number of Devos
     function counter() external view returns (uint256);
 
-    // Function to share a haiku with another address
-    function shareHaiku(uint256 _id, address _to) external;
+    // Function to share a Devo with another address
+    function shareDevo(uint256 _id, address _to) external;
 
-    // Function to get haikus shared with the caller
-    function getMySharedHaikus() external view returns (Haiku[] memory);
+    // Function to get Devos shared with the caller
+    function getMySharedDevos() external view returns (Devo[] memory);
 }
 
-// Contract for managing Haiku NFTs
-contract HaikuNFT is ERC721, ISubmission {
-    Haiku[] public haikus; // Array to store haikus
-    mapping(address => mapping(uint256 => bool)) public sharedHaikus; // Mapping to track shared haikus
-    uint256 public haikuCounter; // Counter for total haikus minted
+// Contract for managing Devo NFTs
+contract DevoNFT is ERC721, ISubmission {
+    Devo[] public Devos; // Array to store Devos
+    mapping(address => mapping(uint256 => bool)) public sharedDevos; // Mapping to track shared Devos
+    uint256 public DevoCounter; // Counter for total Devos minted
 
     // Constructor to initialize the ERC721 contract
-    constructor() ERC721("HaikuNFT", "HAIKU") {
-        haikuCounter = 1; // Initialize haiku counter
+    constructor() ERC721("DevoNFT", "Devo") {
+        DevoCounter = 1; // Initialize Devo counter
     }
 
     string salt = "value"; // A private string variable
 
-    // Function to get the total number of haikus
+    // Function to get the total number of Devos
     function counter() external view override returns (uint256) {
-        return haikuCounter;
+        return DevoCounter;
     }
 
-    // Function to mint a new haiku
-    function mintHaiku(
+    // Function to mint a new Devo
+    function mintDevo(
         string memory _line1,
         string memory _line2,
         string memory _line3
     ) external override {
-        // Check if the haiku is unique
-        string[3] memory haikusStrings = [_line1, _line2, _line3];
-        for (uint256 li = 0; li < haikusStrings.length; li++) {
-            string memory newLine = haikusStrings[li];
-            for (uint256 i = 0; i < haikus.length; i++) {
-                Haiku memory existingHaiku = haikus[i];
-                string[3] memory existingHaikuStrings = [
-                    existingHaiku.line1,
-                    existingHaiku.line2,
-                    existingHaiku.line3
+        // Check if the Devo is unique
+        string[3] memory DevosStrings = [_line1, _line2, _line3];
+        for (uint256 li = 0; li < DevosStrings.length; li++) {
+            string memory newLine = DevosStrings[li];
+            for (uint256 i = 0; i < Devos.length; i++) {
+                Devo memory existingDevo = Devos[i];
+                string[3] memory existingDevoStrings = [
+                    existingDevo.line1,
+                    existingDevo.line2,
+                    existingDevo.line3
                 ];
                 for (uint256 eHsi = 0; eHsi < 3; eHsi++) {
-                    string memory existingHaikuString = existingHaikuStrings[
+                    string memory existingDevoString = existingDevoStrings[
                         eHsi
                     ];
                     if (
-                        keccak256(abi.encodePacked(existingHaikuString)) ==
+                        keccak256(abi.encodePacked(existingDevoString)) ==
                         keccak256(abi.encodePacked(newLine))
                     ) {
-                        revert HaikuNotUnique();
+                        revert DevoNotUnique();
                     }
                 }
             }
         }
 
-        // Mint the haiku NFT
-        _safeMint(msg.sender, haikuCounter);
-        haikus.push(Haiku(msg.sender, _line1, _line2, _line3));
-        haikuCounter++;
+        // Mint the Devo NFT
+        _safeMint(msg.sender, DevoCounter);
+        Devos.push(Devo(msg.sender, _line1, _line2, _line3));
+        DevoCounter++;
     }
 
-    // Function to share a haiku with another address
-    function shareHaiku(uint256 _id, address _to) external override {
-        require(_id > 0 && _id <= haikuCounter, "Invalid haiku ID");
+    // Function to share a Devo with another address
+    function shareDevo(uint256 _id, address _to) external override {
+        require(_id > 0 && _id <= DevoCounter, "Invalid Devo ID");
 
-        Haiku memory haikuToShare = haikus[_id - 1];
-        require(haikuToShare.author == msg.sender, "NotYourHaiku");
+        Devo memory DevoToShare = Devos[_id - 1];
+        require(DevoToShare.author == msg.sender, "NotYourDevo");
 
-        sharedHaikus[_to][_id] = true;
+        sharedDevos[_to][_id] = true;
     }
 
-    // Function to get haikus shared with the caller
-    function getMySharedHaikus()
+    // Function to get Devos shared with the caller
+    function getMySharedDevos()
         external
         view
         override
-        returns (Haiku[] memory)
+        returns (Devo[] memory)
     {
-        uint256 sharedHaikuCount;
-        for (uint256 i = 0; i < haikus.length; i++) {
-            if (sharedHaikus[msg.sender][i + 1]) {
-                sharedHaikuCount++;
+        uint256 sharedDevoCount;
+        for (uint256 i = 0; i < Devos.length; i++) {
+            if (sharedDevos[msg.sender][i + 1]) {
+                sharedDevoCount++;
             }
         }
 
-        Haiku[] memory result = new Haiku[](sharedHaikuCount);
+        Devo[] memory result = new Devo[](sharedDevoCount);
         uint256 currentIndex;
-        for (uint256 i = 0; i < haikus.length; i++) {
-            if (sharedHaikus[msg.sender][i + 1]) {
-                result[currentIndex] = haikus[i];
+        for (uint256 i = 0; i < Devos.length; i++) {
+            if (sharedDevos[msg.sender][i + 1]) {
+                result[currentIndex] = Devos[i];
                 currentIndex++;
             }
         }
 
-        if (sharedHaikuCount == 0) {
-            revert NoHaikusShared();
+        if (sharedDevoCount == 0) {
+            revert NoDevosShared();
         }
 
         return result;
     }
 
     // Custom errors
-    error HaikuNotUnique(); // Error for attempting to mint a non-unique haiku
-    error NotYourHaiku(); // Error for attempting to share a haiku not owned by the caller
-    error NoHaikusShared(); // Error for no haikus shared with the caller
+    error DevoNotUnique(); // Error for attempting to mint a non-unique Devo
+    error NotYourDevo(); // Error for attempting to share a Devo not owned by the caller
+    error NoDevosShared(); // Error for no Devos shared with the caller
 }
